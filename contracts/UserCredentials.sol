744c615cf30c52283621c978899b0e97ca8e3223
Rajveer Singh <rajveersingh05021999@gmail.com>
2024-07-25 18:10:48 +0530

    25-07-2024

--- contracts/UserCredentials.sol
+++ contracts/UserCredentials.sol
@@ -5,41 +5,103 @@ contract UserCredentials {
     struct User {
         string username;
         string passwordHash;
+        string firstname;
+        string lastname;
+        string email;
+        address referral;
     }
 
     mapping(address => User) private users;
     address[] private userAddresses;
 
-    function register(string memory _username, string memory _passwordHash) public {
-        // Only allow registration if the user is not already registered
-        require(bytes(users[msg.sender].username).length == 0, "User already registered");
-
-        users[msg.sender] = User(_username, _passwordHash);
-        userAddresses.push(msg.sender);
-    }
-
-    function getUser() public view returns (string memory username, string memory passwordHash) {
-        User memory user = users[msg.sender];
-        require(bytes(user.username).length != 0, "User not registered");
-        return (user.username, user.passwordHash);
-    }
+    event UserRegistered(
+        address indexed userAddress,
+        string username,
+        string firstname,
+        string lastname,
+        string email,
+        address referral
+    );
 
     function isRegistered() public view returns (bool) {
-        return bytes(users[msg.sender].username).length != 0;
+        return bytes(users[msg.sender].username).length > 0;
     }
 
-    function getAllUsers() public view returns (string[] memory usernames, string[] memory passwordHashes) {
+    function register(
+        string memory _username,
+        string memory _passwordHash,
+        string memory _firstname,
+        string memory _lastname,
+        string memory _email,
+        address _referral
+    ) public {
+        require(!isRegistered(), "User already registered");
+
+        User memory newUser = User({
+            username: _username,
+            passwordHash: _passwordHash,
+            firstname: _firstname,
+            lastname: _lastname,
+            email: _email,
+            referral: _referral
+        });
+
+        users[msg.sender] = newUser;
+        userAddresses.push(msg.sender);
+
+        emit UserRegistered(msg.sender, _username, _firstname, _lastname, _email, _referral);
+    }
+
+    function getUser() public view returns (
+        string memory,
+        string memory,
+        string memory,
+        string memory,
+        string memory,
+        address
+    ) {
+        require(isRegistered(), "User not registered");
+
+        User memory user = users[msg.sender];
+        return (
+            user.username,
+            user.passwordHash,
+            user.firstname,
+            user.lastname,
+            user.email,
+            user.referral
+        );
+    }
+
+    function getAllUsers() public view returns (
+        string[] memory,
+        string[] memory,
+        string[] memory,
+        string[] memory,
+        string[] memory,
+        address[] memory
+    ) {
         uint256 userCount = userAddresses.length;
-        string[] memory usernamesArray = new string[](userCount);
-        string[] memory passwordHashesArray = new string[](userCount);
+        
+        string[] memory usernames = new string[](userCount);
+        string[] memory passwordHashes = new string[](userCount);
+        string[] memory firstnames = new string[](userCount);
+        string[] memory lastnames = new string[](userCount);
+        string[] memory emails = new string[](userCount);
+        address[] memory referrals = new address[](userCount);
 
         for (uint256 i = 0; i < userCount; i++) {
             address userAddress = userAddresses[i];
             User memory user = users[userAddress];
-            usernamesArray[i] = user.username;
-            passwordHashesArray[i] = user.passwordHash;
+
+            usernames[i] = user.username;
+            passwordHashes[i] = user.passwordHash;
+            firstnames[i] = user.firstname;
+            lastnames[i] = user.lastname;
+            emails[i] = user.email;
+            referrals[i] = user.referral;
         }
 
-        return (usernamesArray, passwordHashesArray);
+        return (usernames, passwordHashes, firstnames, lastnames, emails, referrals);
     }
 }
--- scripts/interact.js
+++ scripts/interact.js
@@ -3,36 +3,52 @@ const UserCredentials = artifacts.require("UserCredentials");
 module.exports = async function(callback) {
   try {
     const accounts = await web3.eth.getAccounts();
-    const contractAddress = "0x556F24DA1141f0C912a465D7Fe99B4a2CF4706df"; // Update with the actual address
+    const contractAddress = "0x181B43058fBe2582e4f6a2e6DaD23046f99657Db"; // Update with the actual address
     const userCredentials = await UserCredentials.at(contractAddress);
 
     const username = "testuser3443";
     const passwordHash = "hashed_testpassword34434"; // Provide a hashed password
+    const firstname = "Test";
+    const lastname = "User";
+    const email = "testuser@example.com";
+    const referral = accounts[1]; // Assuming account[1] referred account[0]
 
     // Check if user is already registered
     const isRegistered = await userCredentials.isRegistered.call({ from: accounts[0] });
 
     if (isRegistered) {
       console.log("User already registered:");
-      const [registeredUsername, registeredPasswordHash] = await userCredentials.getUser.call({ from: accounts[0] });
+      const [registeredUsername, registeredPasswordHash, registeredFirstname, registeredLastname, registeredEmail, registeredReferral] = await userCredentials.getUser.call({ from: accounts[0] });
       console.log("Username:", registeredUsername);
       console.log("Password hash:", registeredPasswordHash);
+      console.log("Firstname:", registeredFirstname);
+      console.log("Lastname:", registeredLastname);
+      console.log("Email:", registeredEmail);
+      console.log("Referral:", registeredReferral);
     } else {
       // Register a new user
-      await userCredentials.register(username, passwordHash, { from: accounts[0] });
+      await userCredentials.register(username, passwordHash, firstname, lastname, email, referral, { from: accounts[0] });
       console.log("User registered");
 
       // Get the user information after registration
-      const [newUsername, newPasswordHash] = await userCredentials.getUser.call({ from: accounts[0] });
+      const [newUsername, newPasswordHash, newFirstname, newLastname, newEmail, newReferral] = await userCredentials.getUser.call({ from: accounts[0] });
       console.log("Username after registration:", newUsername);
       console.log("Password hash after registration:", newPasswordHash);
+      console.log("Firstname after registration:", newFirstname);
+      console.log("Lastname after registration:", newLastname);
+      console.log("Email after registration:", newEmail);
+      console.log("Referral after registration:", newReferral);
     }
 
     // Get all users and log in JSON format
-    const [usernames, passwordHashes] = await userCredentials.getAllUsers.call();
+    const [usernames, passwordHashes, firstnames, lastnames, emails, referrals] = await userCredentials.getAllUsers.call();
     const allUsers = usernames.map((username, index) => ({
       username,
-      passwordHash: passwordHashes[index]
+      passwordHash: passwordHashes[index],
+      firstname: firstnames[index],
+      lastname: lastnames[index],
+      email: emails[index],
+      referral: referrals[index]
     }));
 
     console.log("All registered users:", JSON.stringify(allUsers, null, 2));
